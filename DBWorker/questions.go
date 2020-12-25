a026ff04b49fa4e997cc2d9d3e87e37f4f4f3488
aicsoonw <47589137+aicsoonw@users.noreply.github.com>
2020-12-25 07:29:02 +0300

    [T-1612]
    getQ

--- DBWorker/questions.go
+++ DBWorker/questions.go
@@ -1,1 +1,27 @@
+package DBWorker
 
+import (
+	"context"
+	in "github.com/richkule/prepareTestWeb/init"
+)
+
+// Получает все темы теста
+func GetQuestions(id in.TopicId) ([]in.Question, error) {
+	sql := `select id, html, q_type
+	from questions
+	where topic_id = $1`
+	qArr := make([]in.Question, 0)
+	res, err := db.Query(context.Background(), sql, id)
+	if err != nil {
+		return nil, err
+	}
+	for res.Next() {
+		q := in.Question{}
+		err := res.Scan(&q.QuestionId, q.Body, q.QuestionType)
+		if err != nil {
+			return nil, err
+		}
+		qArr = append(qArr, q)
+	}
+	return qArr, nil
+}
