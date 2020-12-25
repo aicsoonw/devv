e0740b84c14c7856eba05e3efe6dc1a9ba754941
aicsoonw <47589137+aicsoonw@users.noreply.github.com>
2020-12-25 07:31:12 +0300

    [T-1626]
    create

--- handlers/tests.go
+++ handlers/tests.go
@@ -1,1 +1,31 @@
+package handlers
+
+import (
+	"errors"
+	db "github.com/richkule/prepareTestWeb/DBWorker"
+	in "github.com/richkule/prepareTestWeb/init"
+	"net/http"
+)
+
+// Обрабатывает страницу create(Создать Тест)
+func Create(w http.ResponseWriter, req *http.Request, sessUs *in.SessUs) error {
+	_ = req // Переменная необходима для совместительства с типом HandlerIdFunc
+	data := in.DataCreateTest{}
+	var err error
+	if data.Header, err = renderHeader(sessUs.UsId); err != nil {
+		err = errors.New("Ошибка обработки шаблона шапки create " + err.Error())
+		return err
+	}
+	data.Tests, err = db.GetTestsById(sessUs.UsId)
+	if err != nil {
+		err = errors.New("Ошибка получения тестов create " + err.Error())
+		return err
+	}
+	err = renderTemplate(w, in.CreateTestPage, data)
+	if err != nil {
+		err = errors.New("Ошибка обработки шаблона create " + err.Error())
+		return err
+	}
+	return nil
+}
 
